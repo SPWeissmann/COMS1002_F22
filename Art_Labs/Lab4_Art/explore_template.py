bbb6a44197f04ef79abe655768441bbcb3756185
Samuel Weissmann <38514250+SPWeissmann@users.noreply.github.com>
2022-09-29 17:56:12 -0400

    rewrote to support bike instead of subway initially

--- Art_Labs/Lab4_Art/explore_template.py
+++ Art_Labs/Lab4_Art/explore_template.py
@@ -4,38 +4,39 @@ spw2136
 
 A program that lets the user explore NYC
 '''
-import subway
+import bicycle as bike
 
 def main ():
-    remaining_fare = 100 #decreases as one rides public transportation
+    energy = 100 #expend 1 energy every two blocks biked.
     location = [0,0] #starting location
 
     choice = 0 #controls the while loop
     while choice != 4:
         #print user status 
         print("You are located at: ", location, sep="")
-        print("Remaining fare: $", remaining_fare, sep="")
+        print("Remaining energy: ", energy, "%", sep="")
 
-        #get user input
+        #get user choice
         display_menu()
         choice = int(input("Select an option: "))
 
         #use chosen transportation method
-        if choice == 1: #riding subway
-            line = input("Pick a line to ride (A, B, C, or D): ")
-            direction = input("Pick a direction to ride (up or down): ")
-            if remaining_fare >= 2.75:
-                remaining_fare -= 2.75 #deduct fare
-                location = subway.ride(location, line, direction) #update location
+        if choice == 1: #riding bike
+            distance = int(input("Enter the number of blocks to ride: "))
+            direction = input("Pick a direction (n, s, e, or w): ")
+            energy_expended = 0.5 * distance
+            if energy_expended <= energy:
+                energy -= energy_expended #deduct energy
+                location = bike.ride(location, distance, direction) #FUNCTION CALL
             else:
-                print("Insufficient fare")
+                print("You are too tired to bike that far")
 
-    return None
+    return None #Not strictly required
 
 #displays a menu of the different transportation options to the user
 def display_menu():
     print("TRANSPORTATION OPTIONS: ")
-    print("1) Ride subway")
+    print("1) Ride Bicycle")
     print("4) Quit")
     return None
 
