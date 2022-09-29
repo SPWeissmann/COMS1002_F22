6233dbcff57c68e3129b642ef9373035c80aec61
Samuel Weissmann <38514250+SPWeissmann@users.noreply.github.com>
2022-09-29 00:45:15 -0400

    minor comment and style edits to make clearer for class

--- Art_Labs/Lab4_Art/subway.py
+++ Art_Labs/Lab4_Art/subway.py
@@ -1,16 +1,19 @@
 # The subway module contains functions relating to subways
 from random import choice #makes choice function from random module available
-LINES = ["A","B","C","D"] 
+LINES = ["A","B","C","D"] #ALL CAPS indicates a variable is a constant
 
 def ride(loc, line, direction):
-    x,y = loc #list expansion. Assigns x = loc[0] and y = loc[1].
+    x = loc[0]
+    y = loc[1]
+    #x,y = loc #list expansion. Assigns x = loc[0] and y = loc[1].
 
-    if line == "A" or line == "D": #A and D are express lines
+    if line == "A" or line == "D": #if an express line
         if direction == "up":
             y += 16
         elif direction == "down":
             y -= 16
-    if line == "B" or line == "C": #B and C are local lines
+
+    if line == "B" or line == "C": #if a local line
         if direction == "up":
             y += 8
         elif direction == "down":
@@ -18,8 +21,13 @@ def ride(loc, line, direction):
     
     location = [x,y]
     return location
+    #return [x,y] #a more succinct way to return the new location.
 
+#randomly generates "next arriving train" and returns the new location
+#after riding it
 def next_train(loc):
     line = choice(LINES)
     direction = choice(["up","down"])
-    return ride(loc, line, direction)
+
+    new_location = ride(loc, line, direction)
+    return new_location
