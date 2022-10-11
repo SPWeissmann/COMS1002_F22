3874891418d44cee29dd9e225b21e5539d87fe27
Samuel Weissmann <38514250+SPWeissmann@users.noreply.github.com>
2022-10-11 18:32:42 -0400

    reformatted for ease of reading

--- Labs/Lab4/args_and_parameters_example.py
+++ Labs/Lab4/args_and_parameters_example.py
@@ -1,8 +1,10 @@
 #Demonstrating basic argument and parameter behaviors
 
 def print_args(para1, para2, para3, para4):
-    #para3 and para4 have default values that will be used if no arguments are passed
-    #default values are indicated with the = operator in the function header
+    # para3 and para4 have default values that will be used if no arguments 
+    # are passed. Default values are indicated with the = operator in the 
+    # function header
+
     print("First argument:", para1)
     print("Second argument:", para2)
     print("Third argument:", para3)
@@ -19,34 +21,23 @@ def main():
     arg4 = "Argument Four"
 
     #arguments are assigned to parameters in order specified
+    #compare the two different print statements to see the difference
     print_args(arg1, arg2, arg3, arg4)
-
     print_args(arg4, arg3, arg2, arg1)
 
+    #default values will be used if no args are passed
+    print_args(arg1, arg2)
 
-
-main()
-
-
-
-
-
-
-
-
-    #if no args are passed for parameters with default values, function
-    #will use those default values instead
-    # print_args(arg1, arg2)
+    #can explicitly assign an argument to a particular parameter like this:
+    print_args(arg1, arg2, para4=arg3)
 
     #you must provide an argument for every parameters without a default value.
     #the following line would throw an error:
     #print_args(arg1)
 
-    #can explicitly assign an argument to a particular parameter like this:
-    # print_args(arg1, arg2, para4=arg3)
-
     #print VS return
     #What do you think this will print? Think about it before uncommenting
     # print(print_args(arg1, arg2, arg3, arg4))
 
-    # return None
+
+main()
